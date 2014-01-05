f67bd30f8bc52eda4f4393a55addb1c4ece10dee
Mark C Mitchell <mark.mitchell.00@gmail.com>
2014-01-05 19:17:15 +1100

    update of .gitignore to ignore database.yml; also, update to static_pages_spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  # let() creates a variable corresponding to its argument. 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
   describe "Home page" do
     it "should have the content 'Sample App'" do
@@ -10,7 +13,7 @@ describe "StaticPages" do
     
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home") #checks for an HTML title with the given content.
+      expect(page).to have_title("#{base_title} | Home") #checks for an HTML title with the given content.
       # note that a substring would work as well: have_title("Home")
     end
     
@@ -24,7 +27,7 @@ describe "StaticPages" do
     
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   
@@ -36,7 +39,7 @@ describe "StaticPages" do
     
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end    
   end
   
